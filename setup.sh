c687ac8cca62c8f854609210f9e8e9ceabc2b611
dawood7919 <61195728+dawood7919@users.noreply.github.com>
2020-11-18 06:10:45 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,6 @@ wget -q https://downloads.rclone.org/v1.52.3/rclone-v1.52.3-linux-amd64.zip
 unzip -q rclone-v1.52.3-linux-amd64.zip
 export PATH=$PWD/rclone-v1.52.3-linux-amd64:$PATH
 #install youtube-dl
+curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+chmod a+rx /usr/local/bin/youtube-dl
 
-sudo wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl
-sudo chmod a+rx /usr/local/bin/youtube-dl
